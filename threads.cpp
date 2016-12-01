4b93f02355ee41f6bb51c3059275fb2bcd6ee495
HannahBraun <hannah.c.braun@vanderbilt.edu>
2016-12-01 14:38:56 -0600

    Update threads.cpp

--- threads.cpp
+++ threads.cpp
@@ -44,10 +44,11 @@ static void *control(void *p) {
 	     char buffer[256];
 	     struct sockaddr_in serv_addr, cli_addr;
 	     int n;
-	     if (argc < 2) {
+		
+	     /*if (argc < 2) {
 	         fprintf(stderr,"ERROR, no port provided\n");
 	         exit(1);
-	     }
+	     }*/
 	     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	     if (sockfd < 0)
 	        error("ERROR opening socket");
@@ -231,7 +232,7 @@ static void *motor(void *p) {
 }
 
 
-int main() {
+int main(int argc, char* argv[]) {
 	/*
 	myCape *capeptr = new myCape();
 	capeptr->DCMotorInit(MotorName, Frequency);
